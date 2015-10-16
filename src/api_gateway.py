50d230b3dd1d6b8e2c817445fbc2518fc37ff87c
Bryan pedlar <pedlar.bryan@gmail.com>
2015-10-15 19:02:57 -0700

    added capability for working with already existing api

--- src/api_gateway.py
+++ src/api_gateway.py
@@ -1,10 +1,16 @@
 from __future__ import print_function
 from sign_request import sign_request
+import os
 
 
 class ApiGatewayConnection:
 
-    def __init__(self, access_key, secret_key):
+    def __init__(self, access_key=None, secret_key=None):
+
+        if not access_key or not secret_key:
+            access_key = os.environ.get('AWS_ACCESS_KEY_ID', access_key)
+            secret_key = os.environ.get('AWS_SECRET_ACCESS_KEY', secret_key)
+
         self.access_key = access_key
         self.secret_key = secret_key
 
@@ -25,14 +31,16 @@ class ApiGatewayConnection:
                             self.secret_key,
                             method='get',
                             canonical_uri='/restapis')
-        return resp.json()
+        return resp
 
     def get_api(self, api_id):
         resp = sign_request(self.access_key,
                             self.secret_key,
                             method='get',
                             canonical_uri='/restapis/{0}'.format(api_id))
-        return resp.json()
+        body = resp.json()
+        return {"api_id": body["resourceCreate"]["restApiId"],
+                "parent_id": body["resourceCreate"]["parentId"]}
 
     def delete_api(self, api_id):
         resp = sign_request(self.access_key,
--- src/main.py
+++ src/main.py
@@ -5,18 +5,45 @@ from argparse import ArgumentParser
 
 
 def get_path_segments(path):
-    parts = filter(None, path.split("/"))
+    parts = filter(None, path.split(os.sep))
     return parts
 
 
-def create_resource_path(api_id, parent_id, path):
+def create_resource_path(api_connection, api_id, parent_id, path):
     segments = get_path_segments(args.path)
+    resources = api_connection.get_resources(api_id)
+
     for segment in segments:
-        create_resource_resp = api_connection.create_resource(
-            api_id, parent_id, segment)
-        resource_json = create_resource_resp.json()
+        
+        previous_created_resource = next(
+            (x for x in resources['item'] if x['pathPart'] == segment
+            and x['parentId'] == parent_id)
+        , None)
+
+        if previous_created_resource:
+            # get child of current parent to find this one
+            resource_json = previous_created_resource
+        else:
+            create_resource_resp = api_connection.create_resource(
+                api_id, parent_id, segment)
+            resource_json = create_resource_resp.json()
+        
         parent_id = resource_json["id"]
+
     return parent_id
+
+
+def create_integrations_and_responses(api_connection, api_id, parent_id, method="get", status_code=200):
+    create_method_resp = api_connection.create_method(
+        api_id, parent_id, method)
+    create_resource_integration = api_connection.create_integration(
+        api_id, parent_id, method, gateway_function_arn,
+        creds_arn)
+    create_integration_response = api_connection.create_integration_response(
+        api_id, parent_id, method, status_code)
+    create_response = api_connection.create_method_response(
+        api_id, parent_id, method, status_code)
+    create_deployment = api_connection.create_deployment(api_id, "test")
 
 
 parser = ArgumentParser(description=str('Create a single endpoint with a '
@@ -25,12 +52,14 @@ parser.add_argument('--zip', type=str, required=True,
                     help='path to the zip file to upload to lambda')
 parser.add_argument("--function_name", type=str, required=True,
                     help="name of the lambda function to upload")
-parser.add_argument("--api_name", type=str, required=True,
-                    help="name of the new api to create")
 parser.add_argument("--path", type=str, required=True,
                     help="path segment to upload to aws")
 parser.add_argument("--handler", type=str, required=True,
                     help="path to your python lambda handler")
+group = parser.add_mutually_exclusive_group()
+group.add_argument("--api_id", type=str, help="the api you wish to update")
+group.add_argument("--api_name", type=str,
+                   help="name of the new api to create")
 
 args = parser.parse_args()
 
@@ -54,24 +83,18 @@ gateway_function_arn = str('arn:aws:apigateway:us-east-1:lambda:path'
                            '/2015-03-31/functions/{0}/invocations').format(
     function_arn)
 
-api_resp = api_connection.create_api(args.api_name)
+
+# shouldn't need a third case here, parser should catch it
+if args.api_name:
+    api_resp = api_connection.create_api(args.api_name)
+elif args.api_id:
+    api_resp = api_connection.get_api(args.api_id)
 
 api_id = api_resp["api_id"]
 parent_id = api_resp["parent_id"]
 
-parent_id = create_resource_path(api_id, parent_id, args.path)
+parent_id = create_resource_path(api_connection, api_id, parent_id, args.path)
 
-create_method_resp = api_connection.create_method(
-    api_id, parent_id, "get")
+create_integrations_and_responses(api_connection, api_id, parent_id)
 
-create_resource_integration = api_connection.create_integration(
-    api_id, parent_id, "get", gateway_function_arn,
-    creds_arn)
-
-create_integration_response = api_connection.create_integration_response(
-    api_id, parent_id, "get", 200)
-
-create_response = api_connection.create_method_response(
-    api_id, parent_id, "get", 200)
-
-create_deployment = api_connection.create_deployment(api_id, "test")
+print("We worked on {0}".format(api_id))
